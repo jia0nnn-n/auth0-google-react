1d88bd302ba36e285895550b144624396affe169
jia0_nn <linao264590000@gmail.com>
2020-10-12 21:40:36 +0800

    fixed url

--- src/Auth.js
+++ src/Auth.js
@@ -5,12 +5,11 @@ import { PROJECT_NAME } from './constant'
 const LOGIN_SUCCESS_PAGE = `${PROJECT_NAME}/user-privacy`;
 const LOGIN_FAILED_PAGE = `${PROJECT_NAME}`;
 
-const host = process.env.ENV === 'prod' ? `https://jia0nnn-n.github.io/auth0-google-react/${PROJECT_NAME}` : `http://localhost:3000/${PROJECT_NAME}`;
 
 const auth = new auth0.WebAuth({
     domain: 'dev--fjtj9b2.us.auth0.com',
     clientID: 'c8AlWkVeRX9ynTeGqVx0re3jbAbC834Y',
-    redirectUri: `${host}/va`,
+    redirectUri: `http://localhost:3000/auth0-google-react/va`,
     audience: 'https://dev--fjtj9b2.us.auth0.com/userinfo',
     responseType: 'token id_token',
     scope: 'openid profile'
--- src/components/Index.js
+++ src/components/Index.js
@@ -6,7 +6,7 @@ function Index(props) {
     return (
         <div>
             <p>Hello {props.state.name}!</p>
-            <a href={`${PROJECT_NAME}/user-privacy`}>Go to my safe zone</a>
+            <a href={`./user-privacy`}>Go to my safe zone</a>
             {
                 !isAuthenticated() && <button onClick={login}>Go Login First</button>
             }
