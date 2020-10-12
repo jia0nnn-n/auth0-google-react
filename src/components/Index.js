42827389567062b028742fa4f782c3748b8cce21
jia0_nn <linao264590000@gmail.com>
2020-10-12 22:13:44 +0800

    keep prefix

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { login, isAuthenticated } from '../Auth';
+import { PROJECT_NAME } from '../constant'
 
 function Index(props) {
     return (
         <div>
             <p>Hello {props.state.name}!</p>
-            <a href={`./user-privacy`}>Go to my safe zone</a>
+            <a href={`${PROJECT_NAME}/user-privacy`}>Go to my safe zone</a>
             {
                 !isAuthenticated() && <button onClick={login}>Go Login First</button>
             }
